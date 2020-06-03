50a0e0afd0d145fb3dd6f503075f5ff6cdb7c688
shilyaev <vvsh1978@mail.ru>
2020-06-03 19:29:56 +0500

    feat: add benefit section

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,56 @@ $(document).ready(function () {
     $('.nav__item:focus').addClass('nav__item--activ').sibling().removeClass('nav__item--activ')
   });
 
+  // Слайдер секции Benefit
+
+  var benefitSwiperPrev = new Swiper('.benefit__swiper-container-prev', {
+    speed: 400,
+    loop: true,
+    spaceBetween: 100,
+    centeredSlides: true,
+    controller: {
+      control: benefitSwiper,
+      by: 'slide',
+    },
+    navigation: {
+      nextEl: '.benefit__button-next',
+      prevEl: '.benefit__button-prev',
+    },
+  });
+
+  var benefitSwiperNext = new Swiper('.benefit__swiper-container-next', {
+    speed: 400,
+    loop: true,
+    spaceBetween: 100,
+    centeredSlides: true,
+    controller: {
+      control: benefitSwiperPrev,
+      by: 'slide',
+    },
+    navigation: {
+      nextEl: '.benefit__button-next',
+      prevEl: '.benefit__button-prev',
+    },
+  });
+
+  var benefitSwiper = new Swiper('.benefit__swiper-container', {
+    speed: 400,
+    loop: true,
+    spaceBetween: 100,
+    centeredSlides: true,
+    controller: {
+      control: benefitSwiperNext,
+      by: 'slide',
+    },
+    navigation: {
+      nextEl: '.benefit__button-next',
+      prevEl: '.benefit__button-prev',
+    },
+  });
 
 
+
+
+  
 
 });
